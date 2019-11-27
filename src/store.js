7b37d803dd8b8efa2e07e88b1f1aa97e3c8acb5f
Alec Champaign <alecchampaign@protonmail.com>
2019-11-27 12:56:35 -0500

    Init store with board

--- src/store.js
+++ src/store.js
@@ -1,7 +1,64 @@
 import { createStore, combineReducers } from 'redux';
 
+const placeMine = () => {
+  const randRowInd = Math.floor(Math.random() * 10);
+  const randColInd = Math.floor(Math.random() * 10)
+
+  return [randRowInd, randColInd];
+}
+
+const board = [
+  [
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false},
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}
+  ],
+  [
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false},
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}
+  ],
+  [
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false},
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}
+  ],
+  [
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false},
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}
+  ],
+  [
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false},
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}
+  ],
+  [
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false},
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}
+  ],
+  [
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false},
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}
+  ],
+  [
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false},
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}
+  ],
+  [
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false},
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}
+  ],
+  [
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false},
+    {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}, {content: '', hasMine: false}
+  ]
+]
+
+for (let i = 0; i < 10; i++) {
+  const mineCoords = placeMine();
+  board[mineCoords[0]][mineCoords[1]].hasMine = true;
+}
+
+console.log(board);
+
 const rootReducer = combineReducers(/* Reducers here */);
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, board);
 
 export default store;
