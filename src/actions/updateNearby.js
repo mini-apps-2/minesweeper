cd424939321181a0b908558cca2f00c84077976f
Alec Champaign <alecchampaign@protonmail.com>
2019-12-01 15:23:39 -0500

    Refactor action

--- src/actions/updateNearby.js
+++ src/actions/updateNearby.js
@@ -1,3 +1,10 @@
-const updateNearby = () => {
-  
+const updateNearby = (numOfMines, cellIndex, rowIndex) => {
+  return {
+    type: 'UPDATE_NEARBY',
+    numOfMines,
+    cellIndex,
+    rowIndex
+  }
 }
+
+export default updateNearby
