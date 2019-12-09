fb18e19cc02b0bbe91ee7b29eee0720a0e05013e
Alec Champaign <alecchampaign@protonmail.com>
2019-12-08 21:15:28 -0500

    Add GET routes

--- server/index.js
+++ server/index.js
@@ -8,12 +8,47 @@ app.use(express.static(path.join(__dirname, '../dist')))
 app.use(parser.json())
 
 app.post('/scores', (req, res) => {
-  console.log(req.body.difficulty, req.body.score)
   connection.query('INSERT INTO scores (scores, difficulty) VALUES(?, ?)', [req.body.score, req.body.difficulty], err => {
     if (err) {
       console.error(err)
       res.sendStatus(400)
     } else res.sendStatus(201)
+  })
+})
+
+app.get('/scores', (req, res) => {
+  connection.query('SELECT scores, difficulty FROM scores', (err, data) => {
+    if (err) {
+      console.log(err)
+      res.sendStatus(404)
+    } else res.status(200).json(data)
+  })
+})
+
+app.get('/scores/easy', (req, res) => {
+  connection.query('SELECT scores, difficulty FROM scores WHERE difficulty="easy"', (err, data) => {
+    if (err) {
+      console.log(err)
+      res.sendStatus(404)
+    } else res.status(200).json(data)
+  })
+})
+
+app.get('/scores/hard', (req, res) => {
+  connection.query('SELECT scores, difficulty FROM scores WHERE difficulty="hard"', (err, data) => {
+    if (err) {
+      console.log(err)
+      res.sendStatus(404)
+    } else res.status(200).json(data)
+  })
+})
+
+app.get('/scores/ludicrous', (req, res) => {
+  connection.query('SELECT scores, difficulty FROM scores WHERE difficulty="ludicrous"', (err, data) => {
+    if (err) {
+      console.log(err)
+      res.sendStatus(404)
+    } else res.status(200).json(data)
   })
 })
 
